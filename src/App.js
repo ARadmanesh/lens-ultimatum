46037b9743f7c2d5bb4032420bf523d0a466fe86
Arman Radmanesh <Radmanesh@gmail.com>
2023-02-28 04:35:19 +0330

    Testing github Action for CI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback, memo} from "react";
+import React, {useState, useEffect, Fragment, useCallback, memo} from "react";
 //import ReactDOM from "react-dom";
 import {
   Typography,
@@ -10,7 +10,6 @@ import {
   Paper,
   Button,
   Avatar,
-  Box,
   Badge
 } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
@@ -20,7 +19,6 @@ import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import { ltrTheme } from "./utils/theme";
 import { DndProvider, useDrag , useDrop} from 'react-dnd';
 import { TouchBackend } from 'react-dnd-touch-backend'
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 //css
 import "./app.css";
@@ -99,102 +97,64 @@ export default function App() {
             <Grid item>
               <Paper className="view-container">
                 {/* Here the study view starts */}
-                <Grid container direction='column' spacing={2} className='ultimatum-container'>
-                  <Grid item spacing={12}>
-                    <Typography variant="h4">rule description</Typography>
-                  </Grid>
-                  <Grid item container direction='row' justify="space-around" alignItems='center'>
-                    <Grid item><Grid container direction='column' justify="space-around" alignItems='center'>
-                      <Typography color='textSecondary' variant='caption'>Trial: 1 of 3</Typography>
-                    </Grid></Grid>
-                  </Grid>
-                  <DndProvider backend={TouchBackend} options={{enableMouseEvents: true}}>
-                    {boxes.map(({name, amount, accepts}, index) => (
-                      <RepositoryBox
-                        accept={accepts}
-                        name={name}
-                        amount={amount}
-                        onDrop={(item) => handleDrop(name, item)}
-                        key={index}
-                      />
-                    ))}
-                  </DndProvider>
+                <Fragment>
+                  <Grid container direction='column' spacing={2} alignItems='stretch' justifyContent='flex-start' className='ultimatum-container'>
+                    <Grid item>
+                      <Typography variant="h4">rule description</Typography>
+                    </Grid>
+                    <Grid item container direction='column' alignItems='stretch'>
+                      <DndProvider backend={TouchBackend} options={{enableMouseEvents: true}}>
+                        {boxes.map(({name, amount, accepts}, index) => (
+                          <RepositoryBox
+                            accept={accepts}
+                            name={name}
+                            amount={amount}
+                            onDrop={(item) => handleDrop(name, item)}
+                            key={index}
+                          />
+                        ))}
+                      </DndProvider>
+                    </Grid>
 
-                  {/* Box */}
-                      <Grid item xs={12}>
-                        <Paper className={classes.paper} elevation={3} >
-                          <Grid container direction="row">
-                            <Grid item xs={4}>
-                                <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
-                                <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
-                                <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
-                            </Grid>
-                            <Grid item xs={8}>
-                              <Grid container direction="row" justifyContent="felx-start" alignItems="center" className={classes.height100}>
-                                <Grid item><MonetizationOnIcon role="Handle" data-test-id={`token`} /> </Grid>
+                    {/* Box */}
+                        <Grid item xs={12}>
+                          <Paper className={classes.paper} elevation={3} >
+                            <Grid container direction="row">
+                              <Grid item xs={4}>
+                                  <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
+                                  <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
+                                  <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
+                                  <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
+                              </Grid>
+                              <Grid item xs={8}>
+                                <Grid container direction="row" justifyContent="felx-start" alignItems="center" className={classes.height100}>
+                                  <Grid item><MonetizationOnIcon role="Handle" data-test-id={`token`} /> </Grid>
+                                </Grid>
                               </Grid>
                             </Grid>
-                          </Grid>
-                          <Grid container direction='row' justifyContent="flex-end" alignItems='center'>
+                            <Grid container direction='row' justifyContent="flex-end" alignItems='center'>
+                              <Grid item>
+                                <Typography variant="body2" color="textSecondary" component="span">Total: 12</Typography>
+                              </Grid>
+                            </Grid>
+                          </Paper>
+                        </Grid>
+                      {/* BOX */}
+                      <Grid item xs={12}>
+                        <Paper className={classes.paper} elevation={3}>
+                          <Grid container direction='row' justifyContent="space-around" alignItems='center'>
                             <Grid item>
-                              <Typography variant="body2" color="textSecondary" component="span">Total: 12</Typography>
+                              <Typography aligin>Player (12)</Typography>
                             </Grid>
                           </Grid>
-                        </Paper>
-                      </Grid>
-                    {/* BOX */}
-                    <Grid item xs={12} spacing={2}>
-                      <Paper className={classes.paper} elevation={3}>
-                        <Grid container direction='row' justifyContent="space-around" alignItems='center'>
-                          <Grid item>
-                            <Typography aligin>Player (12)</Typography>
-                          </Grid>
-                        </Grid>
-                        <Grid container direction="row">
-                          <Grid item xs={4}>
-                            <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
-                            <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
-                          </Grid>
-                          <Grid item xs={8}  justifyContent="center" alignItems="center" className={classes.height100}>
-                              <MonetizationOnIcon key="1" />
-                              <MonetizationOnIcon key="2" />
-                              <MonetizationOnIcon key="3" />
-                              <MonetizationOnIcon key="4" />
-                              <MonetizationOnIcon key="5" />
-                              <MonetizationOnIcon key="6" />
-                              <MonetizationOnIcon key="7" />
-                              <MonetizationOnIcon key="8" />
-                              <MonetizationOnIcon key="9" />
-                              <MonetizationOnIcon key="10" />
-                              <span class="stack-badge">
-                                <Badge badgeContent={2} color="primary">
-                                  <AttachMoneyIcon />
-                                </Badge>
-                              </span>
-                          </Grid>
-                        </Grid>
-                        <Grid container direction='row' justifyContent="space-around" alignItems='center'>
-                          <Grid item>
-                            <Typography aligin>Total: 12</Typography>
-                          </Grid>
-                        </Grid>
-                      </Paper>
-                    </Grid>
-                    {/* BOX */}
-                    <Grid item xs={12} spacing={2}>
-                      <Paper className={classes.paper} elevation={3}>
-                        <Grid container direction='row'>
-                          <Grid item xs={12}>
-                            <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
-                            <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
-                            <Typography align='right'>Player (12)</Typography>
-                          </Grid>
-                          <Grid item  xs={12}>
+                          <Grid container direction="row">
+                            <Grid item xs={4}>
+                              <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
+                              <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
+                              <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
+                              <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
+                            </Grid>
+                            <Grid item xs={8} className={classes.height100}>
                                 <MonetizationOnIcon key="1" />
                                 <MonetizationOnIcon key="2" />
                                 <MonetizationOnIcon key="3" />
@@ -205,55 +165,102 @@ export default function App() {
                                 <MonetizationOnIcon key="8" />
                                 <MonetizationOnIcon key="9" />
                                 <MonetizationOnIcon key="10" />
+                                <span class="stack-badge">
+                                  <Badge badgeContent={2} color="primary">
+                                    <AttachMoneyIcon />
+                                  </Badge>
+                                </span>
+                            </Grid>
                           </Grid>
-                        </Grid>
-                      </Paper>
-                    </Grid>
-                  {/* Box */}
-                    <Grid item xs={12}>
-                      <Paper className={classes.paper}>
-                        <Grid container>
-                          <Grid item xs={12}>
-                            <Typography>Pot (4)</Typography>
+                          <Grid container direction='row' justifyContent="space-around" alignItems='center'>
+                            <Grid item>
+                              <Typography aligin>Total: 12</Typography>
+                            </Grid>
                           </Grid>
-                          <Grid item xs={12}>
-                            <div>
-                              <MonetizationOnIcon key="1" />
-                              <MonetizationOnIcon key="2" />
-                              <MonetizationOnIcon key="3" />
-                              <MonetizationOnIcon key="4" />
-                            </div>
+                        </Paper>
+                      </Grid>
+                      {/* BOX */}
+                      <Grid item xs={12} spacing={2}>
+                        <Paper className={classes.paper} elevation={3}>
+                          <Grid container direction='row'>
+                            <Grid item xs={12}>
+                              <Avatar alt="Marry Stone" src="/images/avatars/marry-avatar.jpg" className={classes.large} />
+                              <Typography variant="body1" color="textPrimary" component="p"> Marry Stone</Typography>
+                              <Typography variant="body2" color="textSecondary" component="p">27 years old</Typography>
+                              <Typography variant="body2" color="textSecondary" component="p">Nurse</Typography>
+                              <Typography align='right'>Player (12)</Typography>
+                            </Grid>
+                            <Grid item  xs={12}>
+                                  <MonetizationOnIcon key="1" />
+                                  <MonetizationOnIcon key="2" />
+                                  <MonetizationOnIcon key="3" />
+                                  <MonetizationOnIcon key="4" />
+                                  <MonetizationOnIcon key="5" />
+                                  <MonetizationOnIcon key="6" />
+                                  <MonetizationOnIcon key="7" />
+                                  <MonetizationOnIcon key="8" />
+                                  <MonetizationOnIcon key="9" />
+                                  <MonetizationOnIcon key="10" />
+                            </Grid>
                           </Grid>
-                          <Grid item xs={12}>
-                            Total: 4
+                        </Paper>
+                      </Grid>
+                    {/* Box */}
+                      <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                          <Grid container>
+                            <Grid item xs={12}>
+                              <Typography>Pot (4)</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                              <div>
+                                <MonetizationOnIcon key="1" />
+                                <MonetizationOnIcon key="2" />
+                                <MonetizationOnIcon key="3" />
+                                <MonetizationOnIcon key="4" />
+                              </div>
+                            </Grid>
+                            <Grid item xs={12}>
+                              Total: 4
+                            </Grid>
                           </Grid>
-                        </Grid>
-                      </Paper>
-                    </Grid>
-                  {/* Box */}
-                    <Grid item xs={12}>
-                      <Paper className="">
-                        <Grid container>
-                          <Grid item xs={12}>
-                            <Typography>Opponent (2)</Typography>
+                        </Paper>
+                      </Grid>
+                    {/* Box */}
+                      <Grid item xs={12}>
+                        <Paper className="">
+                          <Grid container>
+                            <Grid item xs={12}>
+                              <Typography>Opponent (2)</Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                              <div>
+                                <MonetizationOnIcon key="1" />
+                                <MonetizationOnIcon key="2" />
+                              </div>
+                            </Grid>
+                            <Grid item xs={12}>
+                              Total: 2
+                            </Grid>
                           </Grid>
-                          <Grid item xs={12}>
-                            <div>
-                              <MonetizationOnIcon key="1" />
-                              <MonetizationOnIcon key="2" />
-                            </div>
-                          </Grid>
-                          <Grid item xs={12}>
-                            Total: 2
-                          </Grid>
-                        </Grid>
-                      </Paper>
-                    </Grid>
+                        </Paper>
+                      </Grid>
 
-                  <Grid item container direction="row" justify="space-around" alignItems='center'>
-                    <Button size='large' color='primary' variant='outlined' onClick={()=> {console.log('finish')}}>finish</Button>
+                    <Grid item container direction='row' justifyContent="space-around" alignItems='center'>
+                      <Grid item><Grid container direction='column' justifyContent="space-around" alignItems='center'>
+                        <Typography color='textSecondary' variant='caption'>Trial 1 of 3</Typography>
+                      </Grid></Grid>
+
+                      <Grid item><Grid container direction='column' justifyContent="space-around" alignItems='center'>
+                        <Button size='large' color='primary' variant='outlined' onClick={()=> {console.log('finish')}}>Collect</Button>
+                      </Grid></Grid>
+
+                      <Grid item><Grid container direction='column' justifyContent="space-around" alignItems='center'>
+                        <Typography variant="body1">Total: 12</Typography>
+                      </Grid></Grid>
+                    </Grid>
                   </Grid>
-                </Grid>
+                </Fragment>
                 {/* study view ends */}
 
               </Paper>
@@ -315,15 +322,18 @@ const RepositoryBox = memo(function RepositoryBox({
               <Typography>{name}</Typography>
             }
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={7}>
             <Grid container direction="row" justifyContent="felx-start" alignItems="center" className={classes.height100}>
               {tokensList}
             </Grid>
           </Grid>
-        </Grid>
-        <Grid container direction='row' justifyContent="flex-end" alignItems='center'>
-          <Grid item>
-            <Typography variant="body2" color="textSecondary" component="span">Total: {amount}</Typography>
+          <Grid item xs={1}>
+            <Grid container direction="column" justifyContent="center" alignItems="center">
+              {/* <Typography variant="caption" color="textSecondary">{t('ultimatum.box.total_label', { amount: amount })}</Typography> */}
+              <Badge color="secondary" badgeContent={amount} showZero>
+                <MonetizationOnIcon color={name === ItemTypes.PLAYER ? "primary" : "disabled"} fontSize="large" />
+              </Badge>
+            </Grid>
           </Grid>
         </Grid>
       </Paper>
